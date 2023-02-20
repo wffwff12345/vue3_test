99f5c15a602067f2d40c022f0b0c0ef714c6d0b6
wucp <230557657@qq.com>
2023-02-20 18:16:35 +0800

    Merge branch 'feature' into master

--- src/views/test6.ts
+++ src/views/test6.ts
@@ -14,6 +14,10 @@ export interface test4{
     name:string;
     age:number
 }
+export interface test5{
+    name:string;
+    age:number
+}
 export interface test7{
     name:string;
     age:number
